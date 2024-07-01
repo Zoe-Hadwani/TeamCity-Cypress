f28ad79aa860a2afc614f78b54bbdc2f8d1a8749
Zoe <14148060+Zoe-Hadwani@users.noreply.github.com>
2024-07-01 16:38:11 +0100

    Added new test

--- cypress/integration/2-advanced-examples/misc.spec.js
+++ cypress/integration/2-advanced-examples/misc.spec.js
@@ -1,25 +1,25 @@
 /// <reference types="cypress" />
 
-context('Misc', () => {
+context("Misc", () => {
   beforeEach(() => {
-    cy.visit('https://example.cypress.io/commands/misc')
-  })
+    cy.visit("https://example.cypress.io/commands/misc");
+  });
 
-  it('.end() - end the command chain', () => {
+  it(".end() - end the command chain", () => {
     // https://on.cypress.io/end
 
     // cy.end is useful when you want to end a chain of commands
     // and force Cypress to re-query from the root element
-    cy.get('.misc-table').within(() => {
+    cy.get(".misc-table").within(() => {
       // ends the current chain and yields null
-      cy.contains('Cheryl').click().end()
+      cy.contains("Cheryl").click().end();
 
       // queries the entire table again
-      cy.contains('Charles').click()
-    })
-  })
+      cy.contains("Charles").click();
+    });
+  });
 
-  it('cy.exec() - execute a system command', () => {
+  it("cy.exec() - execute a system command", () => {
     // execute a system command.
     // so you can take actions necessary for
     // your test outside the scope of Cypress.
@@ -28,77 +28,252 @@ context('Misc', () => {
     // we can use Cypress.platform string to
     // select appropriate command
     // https://on.cypress/io/platform
-    cy.log(`Platform ${Cypress.platform} architecture ${Cypress.arch}`)
+    cy.log(`Platform ${Cypress.platform} architecture ${Cypress.arch}`);
 
     // on CircleCI Windows build machines we have a failure to run bash shell
     // https://github.com/cypress-io/cypress/issues/5169
     // so skip some of the tests by passing flag "--env circle=true"
-    const isCircleOnWindows = Cypress.platform === 'win32' && Cypress.env('circle')
+    const isCircleOnWindows =
+      Cypress.platform === "win32" && Cypress.env("circle");
 
     if (isCircleOnWindows) {
-      cy.log('Skipping test on CircleCI')
+      cy.log("Skipping test on CircleCI");
 
-      return
+      return;
     }
 
     // cy.exec problem on Shippable CI
     // https://github.com/cypress-io/cypress/issues/6718
-    const isShippable = Cypress.platform === 'linux' && Cypress.env('shippable')
+    const isShippable =
+      Cypress.platform === "linux" && Cypress.env("shippable");
 
     if (isShippable) {
-      cy.log('Skipping test on ShippableCI')
+      cy.log("Skipping test on ShippableCI");
 
-      return
+      return;
     }
 
-    cy.exec('echo Jane Lane')
-      .its('stdout').should('contain', 'Jane Lane')
+    cy.exec("echo Jane Lane").its("stdout").should("contain", "Jane Lane");
 
-    if (Cypress.platform === 'win32') {
-      cy.exec('print cypress.json')
-        .its('stderr').should('be.empty')
+    if (Cypress.platform === "win32") {
+      cy.exec("print cypress.json").its("stderr").should("be.empty");
     } else {
-      cy.exec('cat cypress.json')
-        .its('stderr').should('be.empty')
+      cy.exec("cat cypress.json").its("stderr").should("be.empty");
 
-      cy.exec('pwd')
-        .its('code').should('eq', 0)
+      cy.exec("pwd").its("code").should("eq", 0);
     }
-  })
+  });
 
-  it('cy.focused() - get the DOM element that has focus', () => {
+  it("cy.focused() - get the DOM element that has focus", () => {
     // https://on.cypress.io/focused
-    cy.get('.misc-form').find('#name').click()
-    cy.focused().should('have.id', 'name')
+    cy.get(".misc-form").find("#name").click();
+    cy.focused().should("have.id", "name");
 
-    cy.get('.misc-form').find('#description').click()
-    cy.focused().should('have.id', 'description')
-  })
+    cy.get(".misc-form").find("#description").click();
+    cy.focused().should("have.id", "description");
+  });
 
-  context('Cypress.Screenshot', function () {
-    it('cy.screenshot() - take a screenshot', () => {
+  context("Cypress.Screenshot", function () {
+    it("cy.screenshot() - take a screenshot", () => {
       // https://on.cypress.io/screenshot
-      cy.screenshot('my-image')
-    })
+      cy.screenshot("my-image");
+    });
 
-    it('Cypress.Screenshot.defaults() - change default config of screenshots', function () {
+    it("Cypress.Screenshot.defaults() - change default config of screenshots", function () {
       Cypress.Screenshot.defaults({
-        blackout: ['.foo'],
-        capture: 'viewport',
+        blackout: [".foo"],
+        capture: "viewport",
         clip: { x: 0, y: 0, width: 200, height: 200 },
         scale: false,
         disableTimersAndAnimations: true,
         screenshotOnRunFailure: true,
-        onBeforeScreenshot () { },
-        onAfterScreenshot () { },
-      })
-    })
-  })
+        onBeforeScreenshot() {},
+        onAfterScreenshot() {},
+      });
+    });
+  });
 
-  it('cy.wrap() - wrap an object', () => {
+  it("cy.wrap() - wrap an object", () => {
     // https://on.cypress.io/wrap
-    cy.wrap({ foo: 'bar' })
-      .should('have.property', 'foo')
-      .and('include', 'bar')
-  })
-})
+    cy.wrap({ foo: "bar" })
+      .should("have.property", "foo")
+      .and("include", "bar");
+  });
+});
+/// <reference types="cypress" />
+
+context("Waiting", () => {
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io/commands/waiting");
+  });
+  // BE CAREFUL of adding unnecessary wait times.
+  // https://on.cypress.io/best-practices#Unnecessary-Waiting
+
+  // https://on.cypress.io/wait
+  it("cy.wait() - wait for a specific amount of time", () => {
+    cy.get(".wait-input1").type("Wait 1000ms after typing");
+    cy.wait(1000);
+    cy.get(".wait-input2").type("Wait 1000ms after typing");
+    cy.wait(1000);
+    cy.get(".wait-input3").type("Wait 1000ms after typing");
+    cy.wait(1000);
+  });
+
+  it("cy.wait() - wait for a specific route", () => {
+    // Listen to GET to comments/1
+    cy.intercept("GET", "**/comments/*").as("getComment");
+
+    // we have code that gets a comment when
+    // the button is clicked in scripts.js
+    cy.get(".network-btn").click();
+
+    // wait for GET comments/1
+    cy.wait("@getComment")
+      .its("response.statusCode")
+      .should("be.oneOf", [200, 304]);
+  });
+});
+
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe("example to-do app", () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit("https://example.cypress.io/todo");
+  });
+
+  it("displays two todo items by default", () => {
+    // We use the `cy.get()` command to get all elements that match the selector.
+    // Then, we use `should` to assert that there are two matched items,
+    // which are the two default items.
+    cy.get(".todo-list li").should("have.length", 2);
+
+    // We can go even further and check that the default todos each contain
+    // the correct text. We use the `first` and `last` functions
+    // to get just the first and last matched elements individually,
+    // and then perform an assertion with `should`.
+    cy.get(".todo-list li").first().should("have.text", "Pay electric bill");
+    cy.get(".todo-list li").last().should("have.text", "Walk the dog");
+  });
+
+  it("can add new todo items", () => {
+    // We'll store our item text in a variable so we can reuse it
+    const newItem = "Feed the cat";
+
+    // Let's get the input element and use the `type` command to
+    // input our new list item. After typing the content of our item,
+    // we need to type the enter key as well in order to submit the input.
+    // This input has a data-test attribute so we'll use that to select the
+    // element in accordance with best practices:
+    // https://on.cypress.io/selecting-elements
+    cy.get("[data-test=new-todo]").type(`${newItem}{enter}`);
+
+    // Now that we've typed our new item, let's check that it actually was added to the list.
+    // Since it's the newest item, it should exist as the last element in the list.
+    // In addition, with the two default items, we should have a total of 3 elements in the list.
+    // Since assertions yield the element that was asserted on,
+    // we can chain both of these assertions together into a single statement.
+    cy.get(".todo-list li")
+      .should("have.length", 3)
+      .last()
+      .should("have.text", newItem);
+  });
+
+  it("can check off an item as completed", () => {
+    // In addition to using the `get` command to get an element by selector,
+    // we can also use the `contains` command to get an element by its contents.
+    // However, this will yield the <label>, which is lowest-level element that contains the text.
+    // In order to check the item, we'll find the <input> element for this <label>
+    // by traversing up the dom to the parent element. From there, we can `find`
+    // the child checkbox <input> element and use the `check` command to check it.
+    cy.contains("Pay electric bill")
+      .parent()
+      .find("input[type=checkbox]")
+      .check();
+
+    // Now that we've checked the button, we can go ahead and make sure
+    // that the list element is now marked as completed.
+    // Again we'll use `contains` to find the <label> element and then use the `parents` command
+    // to traverse multiple levels up the dom until we find the corresponding <li> element.
+    // Once we get that element, we can assert that it has the completed class.
+    cy.contains("Pay electric bill")
+      .parents("li")
+      .should("have.class", "completed");
+  });
+
+  context("with a checked task", () => {
+    beforeEach(() => {
+      // We'll take the command we used above to check off an element
+      // Since we want to perform multiple tests that start with checking
+      // one element, we put it in the beforeEach hook
+      // so that it runs at the start of every test.
+      cy.contains("Pay electric bill")
+        .parent()
+        .find("input[type=checkbox]")
+        .check();
+    });
+
+    it("can filter for uncompleted tasks", () => {
+      // We'll click on the "active" button in order to
+      // display only incomplete items
+      cy.contains("Active").click();
+
+      // After filtering, we can assert that there is only the one
+      // incomplete item in the list.
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+        .first()
+        .should("have.text", "Walk the dog");
+
+      // For good measure, let's also assert that the task we checked off
+      // does not exist on the page.
+      cy.contains("Pay electric bill").should("not.exist");
+    });
+
+    it("can filter for completed tasks", () => {
+      // We can perform similar steps as the test above to ensure
+      // that only completed tasks are shown
+      cy.contains("Completed").click();
+
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+        .first()
+        .should("have.text", "Pay electric bill");
+
+      cy.contains("Walk the dog").should("not.exist");
+    });
+
+    it("can delete all completed tasks", () => {
+      // First, let's click the "Clear completed" button
+      // `contains` is actually serving two purposes here.
+      // First, it's ensuring that the button exists within the dom.
+      // This button only appears when at least one task is checked
+      // so this command is implicitly verifying that it does exist.
+      // Second, it selects the button so we can click it.
+      cy.contains("Clear completed").click();
+
+      // Then we can make sure that there is only one element
+      // in the list and our element does not exist
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+        .should("not.have.text", "Pay electric bill");
+
+      // Finally, make sure that the clear button no longer exists.
+      cy.contains("Clear completed").should("not.exist");
+    });
+  });
+});
--- cypress/integration/2-advanced-examples/spies_stubs_clocks.spec.js
+++ cypress/integration/2-advanced-examples/spies_stubs_clocks.spec.js
@@ -2,127 +2,126 @@
 // remove no check once Cypress.sinon is typed
 // https://github.com/cypress-io/cypress/issues/6720
 
-context('Spies, Stubs, and Clock', () => {
-  it('cy.spy() - wrap a method in a spy', () => {
+context("Spies, Stubs, and Clock", () => {
+  it("cy.spy() - wrap a method in a spy", () => {
     // https://on.cypress.io/spy
-    cy.visit('https://example.cypress.io/commands/spies-stubs-clocks')
+    cy.visit("https://example.cypress.io/commands/spies-stubs-clocks");
 
     const obj = {
-      foo () {},
-    }
+      foo() {},
+    };
 
-    const spy = cy.spy(obj, 'foo').as('anyArgs')
+    const spy = cy.spy(obj, "foo").as("anyArgs");
 
-    obj.foo()
+    obj.foo();
 
-    expect(spy).to.be.called
-  })
+    expect(spy).to.be.called;
+  });
 
-  it('cy.spy() retries until assertions pass', () => {
-    cy.visit('https://example.cypress.io/commands/spies-stubs-clocks')
+  it("cy.spy() retries until assertions pass", () => {
+    cy.visit("https://example.cypress.io/commands/spies-stubs-clocks");
 
     const obj = {
       /**
        * Prints the argument passed
        * @param x {any}
-      */
-      foo (x) {
-        console.log('obj.foo called with', x)
+       */
+      foo(x) {
+        console.log("obj.foo called with", x);
       },
-    }
+    };
 
-    cy.spy(obj, 'foo').as('foo')
-
-    setTimeout(() => {
-      obj.foo('first')
-    }, 500)
+    cy.spy(obj, "foo").as("foo");
 
     setTimeout(() => {
-      obj.foo('second')
-    }, 2500)
+      obj.foo("first");
+    }, 500);
 
-    cy.get('@foo').should('have.been.calledTwice')
-  })
+    setTimeout(() => {
+      obj.foo("second");
+    }, 2500);
 
-  it('cy.stub() - create a stub and/or replace a function with stub', () => {
+    cy.get("@foo").should("have.been.calledTwice");
+  });
+
+  it("cy.stub() - create a stub and/or replace a function with stub", () => {
     // https://on.cypress.io/stub
-    cy.visit('https://example.cypress.io/commands/spies-stubs-clocks')
+    cy.visit("https://example.cypress.io/commands/spies-stubs-clocks");
 
     const obj = {
       /**
        * prints both arguments to the console
        * @param a {string}
        * @param b {string}
-      */
-      foo (a, b) {
-        console.log('a', a, 'b', b)
+       */
+      foo(a, b) {
+        console.log("a", a, "b", b);
       },
-    }
+    };
 
-    const stub = cy.stub(obj, 'foo').as('foo')
+    const stub = cy.stub(obj, "foo").as("foo");
 
-    obj.foo('foo', 'bar')
+    obj.foo("foo", "bar");
 
-    expect(stub).to.be.called
-  })
+    expect(stub).to.be.called;
+  });
 
-  it('cy.clock() - control time in the browser', () => {
+  it("cy.clock() - control time in the browser", () => {
     // https://on.cypress.io/clock
 
     // create the date in UTC so its always the same
     // no matter what local timezone the browser is running in
-    const now = new Date(Date.UTC(2017, 2, 14)).getTime()
+    const now = new Date(Date.UTC(2017, 2, 14)).getTime();
 
-    cy.clock(now)
-    cy.visit('https://example.cypress.io/commands/spies-stubs-clocks')
-    cy.get('#clock-div').click()
-      .should('have.text', '1489449600')
-  })
+    cy.clock(now);
+    cy.visit("https://example.cypress.io/commands/spies-stubs-clocks");
+    cy.get("#clock-div").click().should("have.text", "1489449600");
+  });
 
-  it('cy.tick() - move time in the browser', () => {
+  it("cy.tick() - move time in the browser", () => {
     // https://on.cypress.io/tick
 
     // create the date in UTC so its always the same
     // no matter what local timezone the browser is running in
-    const now = new Date(Date.UTC(2017, 2, 14)).getTime()
+    const now = new Date(Date.UTC(2017, 2, 14)).getTime();
 
-    cy.clock(now)
-    cy.visit('https://example.cypress.io/commands/spies-stubs-clocks')
-    cy.get('#tick-div').click()
-      .should('have.text', '1489449600')
+    cy.clock(now);
+    cy.visit("https://example.cypress.io/commands/spies-stubs-clocks");
+    cy.get("#tick-div").click().should("have.text", "1489449600");
 
-    cy.tick(10000) // 10 seconds passed
-    cy.get('#tick-div').click()
-      .should('have.text', '1489449610')
-  })
+    cy.tick(10000); // 10 seconds passed
+    cy.get("#tick-div").click().should("have.text", "1489449610");
+  });
 
-  it('cy.stub() matches depending on arguments', () => {
+  it("cy.stub() matches depending on arguments", () => {
     // see all possible matchers at
     // https://sinonjs.org/releases/latest/matchers/
     const greeter = {
       /**
        * Greets a person
        * @param {string} name
-      */
-      greet (name) {
-        return `Hello, ${name}!`
+       */
+      greet(name) {
+        return `Hello, ${name}!`;
       },
-    }
+    };
 
-    cy.stub(greeter, 'greet')
+    cy.stub(greeter, "greet")
       .callThrough() // if you want non-matched calls to call the real method
-      .withArgs(Cypress.sinon.match.string).returns('Hi')
-      .withArgs(Cypress.sinon.match.number).throws(new Error('Invalid name'))
+      .withArgs(Cypress.sinon.match.string)
+      .returns("Hi")
+      .withArgs(Cypress.sinon.match.number)
+      .throws(new Error("Invalid name"));
 
-    expect(greeter.greet('World')).to.equal('Hi')
-    expect(() => greeter.greet(42)).to.throw('Invalid name')
-    expect(greeter.greet).to.have.been.calledTwice
+    expect(greeter.greet("World")).to.equal("Hi");
+    expect(() => greeter.greet(42)).to.throw("Invalid name");
+    expect(greeter.greet).to.have.been.calledTwice;
 
     // non-matched calls goes the actual method
-    expect(greeter.greet()).to.equal('Hello, undefined!')
-  })
+    expect(greeter.greet()).to.equal("Hello, undefined!");
+  });
 
-  it('matches call arguments using Sinon matchers', () => {
+  it("matches call arguments using Sinon matchers", () => {
     // see all possible matchers at
     // https://sinonjs.org/releases/latest/matchers/
     const calculator = {
@@ -130,74 +129,193 @@ context('Spies, Stubs, and Clock', () => {
        * returns the sum of two arguments
        * @param a {number}
        * @param b {number}
-      */
-      add (a, b) {
-        return a + b
+       */
+      add(a, b) {
+        return a + b;
       },
-    }
+    };
 
-    const spy = cy.spy(calculator, 'add').as('add')
+    const spy = cy.spy(calculator, "add").as("add");
 
-    expect(calculator.add(2, 3)).to.equal(5)
+    expect(calculator.add(2, 3)).to.equal(5);
 
     // if we want to assert the exact values used during the call
-    expect(spy).to.be.calledWith(2, 3)
+    expect(spy).to.be.calledWith(2, 3);
 
     // let's confirm "add" method was called with two numbers
-    expect(spy).to.be.calledWith(Cypress.sinon.match.number, Cypress.sinon.match.number)
+    expect(spy).to.be.calledWith(
+      Cypress.sinon.match.number,
+      Cypress.sinon.match.number
+    );
 
     // alternatively, provide the value to match
-    expect(spy).to.be.calledWith(Cypress.sinon.match(2), Cypress.sinon.match(3))
+    expect(spy).to.be.calledWith(
+      Cypress.sinon.match(2),
+      Cypress.sinon.match(3)
+    );
 
     // match any value
-    expect(spy).to.be.calledWith(Cypress.sinon.match.any, 3)
+    expect(spy).to.be.calledWith(Cypress.sinon.match.any, 3);
 
     // match any value from a list
-    expect(spy).to.be.calledWith(Cypress.sinon.match.in([1, 2, 3]), 3)
+    expect(spy).to.be.calledWith(Cypress.sinon.match.in([1, 2, 3]), 3);
 
     /**
      * Returns true if the given number is even
      * @param {number} x
      */
-    const isEven = (x) => x % 2 === 0
+    const isEven = (x) => x % 2 === 0;
 
     // expect the value to pass a custom predicate function
     // the second argument to "sinon.match(predicate, message)" is
     // shown if the predicate does not pass and assertion fails
-    expect(spy).to.be.calledWith(Cypress.sinon.match(isEven, 'isEven'), 3)
+    expect(spy).to.be.calledWith(Cypress.sinon.match(isEven, "isEven"), 3);
 
     /**
      * Returns a function that checks if a given number is larger than the limit
      * @param {number} limit
      * @returns {(x: number) => boolean}
      */
-    const isGreaterThan = (limit) => (x) => x > limit
+    const isGreaterThan = (limit) => (x) => x > limit;
 
     /**
      * Returns a function that checks if a given number is less than the limit
      * @param {number} limit
      * @returns {(x: number) => boolean}
      */
-    const isLessThan = (limit) => (x) => x < limit
+    const isLessThan = (limit) => (x) => x < limit;
 
     // you can combine several matchers using "and", "or"
     expect(spy).to.be.calledWith(
       Cypress.sinon.match.number,
-      Cypress.sinon.match(isGreaterThan(2), '> 2').and(Cypress.sinon.match(isLessThan(4), '< 4')),
-    )
+      Cypress.sinon
+        .match(isGreaterThan(2), "> 2")
+        .and(Cypress.sinon.match(isLessThan(4), "< 4"))
+    );
 
     expect(spy).to.be.calledWith(
       Cypress.sinon.match.number,
-      Cypress.sinon.match(isGreaterThan(200), '> 200').or(Cypress.sinon.match(3)),
-    )
+      Cypress.sinon
+        .match(isGreaterThan(200), "> 200")
+        .or(Cypress.sinon.match(3))
+    );
 
     // matchers can be used from BDD assertions
-    cy.get('@add').should('have.been.calledWith',
-      Cypress.sinon.match.number, Cypress.sinon.match(3))
+    cy.get("@add").should(
+      "have.been.calledWith",
+      Cypress.sinon.match.number,
+      Cypress.sinon.match(3)
+    );
 
     // you can alias matchers for shorter test code
-    const { match: M } = Cypress.sinon
+    const { match: M } = Cypress.sinon;
 
-    cy.get('@add').should('have.been.calledWith', M.number, M(3))
-  })
-})
+    cy.get("@add").should("have.been.calledWith", M.number, M(3));
+  });
+});
+/// <reference types="cypress" />
+
+context("Querying", () => {
+  beforeEach(() => {
+    cy.visit("https://example.cypress.io/commands/querying");
+  });
+
+  // The most commonly used query is 'cy.get()', you can
+  // think of this like the '$' in jQuery
+
+  it("cy.get() - query DOM elements", () => {
+    // https://on.cypress.io/get
+
+    cy.get("#query-btn").should("contain", "Button");
+
+    cy.get(".query-btn").should("contain", "Button");
+
+    cy.get("#querying .well>button:first").should("contain", "Button");
+    //              ↲
+    // Use CSS selectors just like jQuery
+
+    cy.get('[data-test-id="test-example"]').should("have.class", "example");
+
+    // 'cy.get()' yields jQuery object, you can get its attribute
+    // by invoking `.attr()` method
+    cy.get('[data-test-id="test-example"]')
+      .invoke("attr", "data-test-id")
+      .should("equal", "test-example");
+
+    // or you can get element's CSS property
+    cy.get('[data-test-id="test-example"]')
+      .invoke("css", "position")
+      .should("equal", "static");
+
+    // or use assertions directly during 'cy.get()'
+    // https://on.cypress.io/assertions
+    cy.get('[data-test-id="test-example"]')
+      .should("have.attr", "data-test-id", "test-example")
+      .and("have.css", "position", "static");
+  });
+
+  it("cy.contains() - query DOM elements with matching content", () => {
+    // https://on.cypress.io/contains
+    cy.get(".query-list").contains("bananas").should("have.class", "third");
+
+    // we can pass a regexp to `.contains()`
+    cy.get(".query-list").contains(/^b\w+/).should("have.class", "third");
+
+    cy.get(".query-list").contains("apples").should("have.class", "first");
+
+    // passing a selector to contains will
+    // yield the selector containing the text
+    cy.get("#querying")
+      .contains("ul", "oranges")
+      .should("have.class", "query-list");
+
+    cy.get(".query-button").contains("Save Form").should("have.class", "btn");
+  });
+
+  it(".within() - query DOM elements within a specific element", () => {
+    // https://on.cypress.io/within
+    cy.get(".query-form").within(() => {
+      cy.get("input:first").should("have.attr", "placeholder", "Email");
+      cy.get("input:last").should("have.attr", "placeholder", "Password");
+    });
+  });
+
+  it("cy.root() - query the root DOM element", () => {
+    // https://on.cypress.io/root
+
+    // By default, root is the document
+    cy.root().should("match", "html");
+
+    cy.get(".query-ul").within(() => {
+      // In this within, the root is now the ul DOM element
+      cy.root().should("have.class", "query-ul");
+    });
+  });
+
+  it("best practices - selecting elements", () => {
+    // https://on.cypress.io/best-practices#Selecting-Elements
+    cy.get("[data-cy=best-practices-selecting-elements]").within(() => {
+      // Worst - too generic, no context
+      cy.get("button").click();
+
+      // Bad. Coupled to styling. Highly subject to change.
+      cy.get(".btn.btn-large").click();
+
+      // Average. Coupled to the `name` attribute which has HTML semantics.
+      cy.get("[name=submission]").click();
+
+      // Better. But still coupled to styling or JS event listeners.
+      cy.get("#main").click();
+
+      // Slightly better. Uses an ID but also ensures the element
+      // has an ARIA role attribute
+      cy.get("#main[role=button]").click();
+
+      // Much better. But still coupled to text content that may change.
+      cy.contains("Submit").click();
+
+      // Best. Insulated from all changes.
+      cy.get("[data-cy=submit]").click();
+    });
+  });
+});
